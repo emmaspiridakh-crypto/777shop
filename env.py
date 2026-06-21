a278f4a2917ec1b42dd4f8b9d7371c78199a052c
emma sp <emmaspiridakh@gmail.com>
2026-06-21 03:42:31 +0300

    Update and rename env.example to env.py

--- env.py
+++ env.py
@@ -1,52 +1,48 @@
-# ============================================
-# BOT TOKEN
-# ============================================
-BOT_TOKEN=
 
 # ============================================
 # STAFF / OWNERSHIP ROLE IDs
 # ============================================
-STAFF_ROLE_ID=
-MANAGER_ROLE_ID=
-CEO_ROLE_ID=
-COO_ROLE_ID=
-CTO_ROLE_ID=
+STAFF_ROLE_ID= 1516091289588994189
+MANAGER_ROLE_ID= 1516083138000977950
+CEO_ROLE_ID= 1516081574251073536
+COO_ROLE_ID= 1516082608159592624
+CTO_ROLE_ID= 1516083051602645102
 
 # ============================================
 # AUTOROLE (δίνεται αυτόματα σε νέα μέλη)
 # ============================================
-AUTOROLE_ID=
+AUTOROLE_ID= 1516091504655864020
 
 # ============================================
 # TICKET SYSTEM - SUPPORT
 # ============================================
-SUPPORT_CATEGORY_ID=
-SUPPORT_PANEL_CHANNEL_ID=
-SUPPORT_THUMBNAIL_URL=https://example.com/support.png
+SUPPORT_CATEGORY_ID= 1518052456016117863
+SUPPORT_PANEL_CHANNEL_ID= 1502388829112500235
+SUPPORT_THUMBNAIL_URL=https://i.imgur.com/iFsMZTo.png
 
 # ============================================
 # TICKET SYSTEM - ORDER
 # ============================================
-ORDER_CATEGORY_ID=
-ORDER_PANEL_CHANNEL_ID=
-ORDER_THUMBNAIL_URL=https://example.com/order.png
+ORDER_CATEGORY_ID= 1518052510483615784
+ORDER_PANEL_CHANNEL_ID= 1502386872645845092
+ORDER_THUMBNAIL_URL=https://i.imgur.com/iFsMZTo.png
 
 # ============================================
 # TICKET LOGS (open/close - support & order)
 # ============================================
-TICKET_LOG_CHANNEL_ID=
+TICKET_LOG_CHANNEL_ID= 1518043566885048361
 
 # ============================================
 # LOGGING CHANNELS
 # ============================================
-JOIN_LEAVE_LOG_CHANNEL_ID=
-VOICE_LOG_CHANNEL_ID=
-MESSAGE_LOG_CHANNEL_ID=
-CHANNEL_LOG_CHANNEL_ID=
-ROLES_LOG_CHANNEL_ID=
-KICK_LOG_CHANNEL_ID=
-TIMEOUT_LOG_CHANNEL_ID=
-BAN_LOG_CHANNEL_ID=
+JOIN_LEAVE_LOG_CHANNEL_ID= 1518040428056936518
+VOICE_LOG_CHANNEL_ID= 1518040513675264061
+MESSAGE_LOG_CHANNEL_ID= 1518040479629971599
+CHANNEL_LOG_CHANNEL_ID= 1518040539704852610
+ROLES_LOG_CHANNEL_ID= 1518040623125630987
+KICK_LOG_CHANNEL_ID= 1518040732097843251
+TIMEOUT_LOG_CHANNEL_ID= 1518040766566633503
+BAN_LOG_CHANNEL_ID= 1518040711461601310
 
 # ============================================
 # WEB SERVER (Render keepalive)
